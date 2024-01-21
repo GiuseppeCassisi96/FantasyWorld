02c8dac5aa034be4cd35d0aeb44fae8d213d264f
Giuseppe cassisi <giuseppe.cassisi@studenti.unimi.it>
2024-01-21 11:50:06 +0100

    Fighter input movement
    feat: I implemented the movement input system for my fighter. Only keyboard is supported

--- Source/FantasyWorld/Private/MainChar/Fighter.cpp
+++ Source/FantasyWorld/Private/MainChar/Fighter.cpp
@@ -3,18 +3,34 @@
 
 #include "MainChar/Fighter.h"
 
+#include "AIHelpers.h"
+#include "GameFramework/MovementComponent.h"
+
 // Sets default values
 AFighter::AFighter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	CameraComponent->SetupAttachment(SpringArmComponent);
 }
 
 // Called when the game starts or when spawned
 void AFighter::BeginPlay()
 {
 	Super::BeginPlay();
+	initialSpeed = speed;
+	if(APlayerController* Player = Cast<APlayerController>(GetController()))
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::
+			GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(Player->GetLocalPlayer()))
+		{
+				InputSubsystem->AddMappingContext(InputMappingContext.LoadSynchronous(), 1);
+		}
+	}
+	
 }
 
 // Called every frame
@@ -28,6 +44,36 @@ void AFighter::Tick(float DeltaTime)
 void AFighter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	Input->BindAction(MoveAction.Get(), ETriggerEvent::Triggered, this, &AFighter::Move);
+	Input->BindAction(RotateAction.Get(), ETriggerEvent::Triggered, this, &AFighter::Rotate);
+	Input->BindAction(RunAction.Get(), ETriggerEvent::Triggered, this, &AFighter::Run);
+	Input->BindAction(RunAction.Get(), ETriggerEvent::Completed, this, &AFighter::NotRun);}
 
+void AFighter::Move(const FInputActionInstance& Instance)
+{
+	FVector2D inputValue = Instance.GetValue().Get<FVector2D>();
+	FVector WorldDir = (inputValue.X * GetActorForwardVector() + inputValue.Y * GetActorRightVector()) * GetWorld()->GetDeltaSeconds();
+	AddMovementInput(WorldDir,speed);
 }
+
+void AFighter::Rotate(const FInputActionInstance& Instance)
+{
+	FVector2D inputValue = Instance.GetValue().Get<FVector2D>();
+	AddControllerYawInput(inputValue.X * rotationSpeed * GetWorld()->GetDeltaSeconds());
+}
+
+void AFighter::Run(const FInputActionInstance& Instance)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, FString::Printf(TEXT("Run")));
+	speed = initialSpeed + 140.0f;
+}
+
+void AFighter::NotRun(const FInputActionInstance& Instance)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, FString::Printf(TEXT("Not Run")));
+	speed = initialSpeed;
+}
+
+
 
--- Source/FantasyWorld/Public/MainChar/Fighter.h
+++ Source/FantasyWorld/Public/MainChar/Fighter.h
@@ -3,7 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "InputMappingContext.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "Fighter.generated.h"
 
 UCLASS()
@@ -12,18 +17,40 @@ class FANTASYWORLD_API AFighter : public ACharacter
 	GENERATED_BODY()
 
 public:
+
+	UPROPERTY(EditDefaultsOnly, Category= "Camera")
+	TObjectPtr<UCameraComponent> CameraComponent;
+	UPROPERTY(EditDefaultsOnly, Category= "Camera")
+	TObjectPtr<USpringArmComponent> SpringArmComponent;
+	UPROPERTY(EditDefaultsOnly, Category= "Input")
+	TSoftObjectPtr<UInputMappingContext> InputMappingContext;
+	UPROPERTY(EditDefaultsOnly, Category= "Input")
+	TSoftObjectPtr<UInputAction> MoveAction;
+	UPROPERTY(EditDefaultsOnly, Category= "Input")
+	TSoftObjectPtr<UInputAction> RotateAction;
+	UPROPERTY(EditDefaultsOnly, Category= "Input")
+	TSoftObjectPtr<UInputAction> RunAction;
+	UPROPERTY(EditDefaultsOnly, Category= "Movement")
+	float speed = 20.0f;
+	UPROPERTY(EditDefaultsOnly, Category= "Movement")
+	float rotationSpeed = 10.0f;
+	float initialSpeed;
 	// Sets default values for this character's properties
 	AFighter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	void Move(const FInputActionInstance& Instance);
+	void Rotate(const FInputActionInstance& Instance);
+	void Run(const FInputActionInstance& Instance);
+	void NotRun(const FInputActionInstance& Instance);
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
 
 };
