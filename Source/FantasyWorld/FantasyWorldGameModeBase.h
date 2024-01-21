4abd71b48357b1caad8ea82a1c6ea8328c88c157
Giuseppe cassisi <giuseppe.cassisi@studenti.unimi.it>
2024-01-21 10:11:55 +0100

    Init Setup

---
+++ Source/FantasyWorld/FantasyWorldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FantasyWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FANTASYWORLD_API AFantasyWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
